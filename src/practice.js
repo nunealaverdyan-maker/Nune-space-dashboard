d5c8f78e03f52e44b6ad862adfca02c09d66c6d7
Student <nune.alaverdyan@tumo.org>
2026-05-19 20:18:11 +0400

    describe what you did

--- src/practice.js
+++ src/practice.js
@@ -19,11 +19,31 @@
 // console.log(describeAstronaut("Alice", "currently aboard the ISS"))
 
 
-const issPosition = {
-    latitude: 800,
-    longitude: 750   
-}
+// const issPosition = {
+//     latitude: 800,
+//     longitude: 750   
+// }
 
-console.log(issPosition)
-console.log(issPosition.latitude)
-console.log(issPosition.longitude)
+// console.log(issPosition)
+// console.log(issPosition.latitude)
+// console.log(issPosition.longitude)
+
+
+const asteroids = [
+    { name: "2024 AB1", diameter: 120, hazardous: false},
+    { name: "2024 CD2", diameter: 45, hazardous: true },
+    { name: "2024 EF3", diameter: 890, hazardous: false },
+    { name: "2024 GH4", diameter: 23, hazardous: true }
+]
+
+const names = asteroids.map(person => person.name)
+console.log(names)
+
+const issOnly = asteroids.filter(person => person.diameter)
+console.log(issOnly)
+
+const firstTwo = asteroids.slice(0, 2)
+console.log(firstTwo)
+
+const hazardousnames = asteroids.filter(person => person.hazardous).map(person => person.name)
+console.log(hazardousnames)
