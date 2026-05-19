56605289d23ffa7173efb432227e76ed9d85199c
Student <nune.alaverdyan@tumo.org>
2026-05-19 19:50:19 +0400

    describe what you did

---
+++ src/practice.js
@@ -0,0 +1,29 @@
+// const name = "Nune"
+// let count = 0
+
+// console.log(name)
+// console.log(count)
+
+
+// function formatmiles(km) {
+//     return km * 0.621
+// }
+
+// console.log(formatmiles(408))
+
+
+// function describeAstronaut(name,craft) {
+//     return name + " is " + craft
+// }
+
+// console.log(describeAstronaut("Alice", "currently aboard the ISS"))
+
+
+const issPosition = {
+    latitude: 800,
+    longitude: 750   
+}
+
+console.log(issPosition)
+console.log(issPosition.latitude)
+console.log(issPosition.longitude)
