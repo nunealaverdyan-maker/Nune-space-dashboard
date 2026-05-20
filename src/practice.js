a99286895cbf6e9a24aaef25bb650d1f46ca30a9
Student <nune.alaverdyan@tumo.org>
2026-05-20 20:10:42 +0400

    may 20

--- src/practice.js
+++ src/practice.js
@@ -29,21 +29,23 @@
 // console.log(issPosition.longitude)
 
 
-const asteroids = [
-    { name: "2024 AB1", diameter: 120, hazardous: false},
-    { name: "2024 CD2", diameter: 45, hazardous: true },
-    { name: "2024 EF3", diameter: 890, hazardous: false },
-    { name: "2024 GH4", diameter: 23, hazardous: true }
-]
+// const asteroids = [
+//     { name: "2024 AB1", diameter: 120, hazardous: false},
+//     { name: "2024 CD2", diameter: 45, hazardous: true },
+//     { name: "2024 EF3", diameter: 890, hazardous: false },
+//     { name: "2024 GH4", diameter: 23, hazardous: true }
+// ]
 
-const names = asteroids.map(person => person.name)
-console.log(names)
+// const names = asteroids.map(person => person.name)
+// console.log(names)
 
-const issOnly = asteroids.filter(person => person.diameter)
-console.log(issOnly)
+// const issOnly = asteroids.filter(person => person.diameter)
+// console.log(issOnly)
 
-const firstTwo = asteroids.slice(0, 2)
-console.log(firstTwo)
+// const firstTwo = asteroids.slice(0, 2)
+// console.log(firstTwo)
 
-const hazardousnames = asteroids.filter(person => person.hazardous).map(person => person.name)
-console.log(hazardousnames)
+// const hazardousnames = asteroids.filter(person => person.hazardous).map(person => person.name)
+// console.log(hazardousnames)
+
+
