3dbdd399c8138ba4b80e8dc2cf458f4392671e45
Student <nune.alaverdyan@tumo.org>
2026-05-21 20:08:21 +0400

    describe what you did

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
  import './App.css'
+ import { useState } from 'react'
  
+
+ function Counter() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      Clicked {count} times
+    </button>
+  )
+ }
+
+
+ function ISSTracker(){
+  const [location,setLocation] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.wheretheiss.at/v1/satellites/25544')
+  .then(r=> r.json())
+  .then(data => setLocation(data))
+  }, [])
+  return (
+    <div className='card'>
+      <h2>ISS Position</h2>
+      { location ?(
+        <p>{location.latitude.toFixed(2)}°,{location.longitude.toFixed(2)}°</p>
+      ):(
+        <p>Loading ...</p>
+      )}
+    </div>
+  )
+}
  
  function ISSCard(){
   return (
@@ -29,8 +61,9 @@
       <ISSCard />
       <MissionBadge />
       <ISSCard latitude = "42.36" longitude="-71.05" />
-<MissionBadge name = "Artemis II" status="Active" />
-
+      <MissionBadge name = "Artemis II" status="Active" />
+      <Counter />
+      <ISSTracker />
     </div>
     
    )
